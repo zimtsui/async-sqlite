e9ec9b23afa417d3d877977f26b6d48f861842a8
Zim <zimtsui@gmail.com>
2019-10-15 22:18:08 +0800

    dev

---
+++ dist/index.d.ts
@@ -0,0 +1,10 @@
+declare class Database {
+    private filepath;
+    private db;
+    constructor(filepath: string);
+    start(): Promise<void>;
+    stop(): Promise<void>;
+    sql(template: any, ...params: any[]): Promise<any>;
+}
+export default Database;
+export { Database };
---
+++ dist/index.js
@@ -0,0 +1,32 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const sqlite3_1 = __importDefault(require("sqlite3"));
+const bluebird_1 = require("bluebird");
+const events_1 = require("events");
+const util_1 = require("util");
+sqlite3_1.default.verbose();
+class Database {
+    constructor(filepath) {
+        this.filepath = filepath;
+    }
+    async start() {
+        this.db = new sqlite3_1.default.Database(this.filepath);
+        await events_1.once(this.db, 'open');
+        bluebird_1.promisifyAll(this.db);
+        this.db.configure('busyTimeout', 1000);
+        await this.db.serializeAsync();
+    }
+    async stop() {
+        await this.db.closeAsync();
+    }
+    async sql(template, ...params) {
+        const statement = util_1.format(template, ...params);
+        return await this.db.allAsync(statement);
+    }
+}
+exports.Database = Database;
+exports.default = Database;
+//# sourceMappingURL=index.js.map
